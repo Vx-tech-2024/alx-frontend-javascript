85b33dd0c5230aa0d05e400ad845fc13906e782b
Vx-tech <xaviervianney8@gmail.com>
2025-11-11 12:03:51 +0300

    Setting up the alx frontend javascript folder

---
+++ 0x04-Typescript/task_1/js/main.ts
@@ -0,0 +1,20 @@
+// task_1/main.ts
+
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // Allows adding any other properties like `contract`
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false, // This works because of [key: string]: any
+};
+
+console.log(teacher3);
