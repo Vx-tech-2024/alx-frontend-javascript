5c14d728f1cc40829d8d612d4fb620d4a1099714
Vx-tech <xaviervianney8@gmail.com>
2025-11-12 12:24:06 +0300

    modifying the subject class

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -150,3 +150,20 @@ function executeWork(employee: Director | Teacher): string {
 // Example usage
 console.log(executeWork(createEmployee(200)));  // Getting to work
 console.log(executeWork(createEmployee(1000))); // Getting to director tasks
+
+
+// Define a string literal type
+type Subjects = 'Math' | 'History';
+
+// Define the function teachClass
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else {
+    return 'Teaching History';
+  }
+}
+
+// Example usage
+console.log(teachClass('Math'));     // Teaching Math
+console.log(teachClass('History'));  // Teaching History
