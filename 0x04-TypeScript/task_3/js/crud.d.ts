4558fa7ac32943d297740a8acd1b525694845ba3
Vx-tech <xaviervianney8@gmail.com>
2025-11-12 12:18:51 +0300

    Changes on interface and crud

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -2,6 +2,6 @@
 
 import { RowID, RowElement } from '../interface';
 
-export function insertRow(row: RowElement): RowID;
-export function deleteRow(rowId: RowID): void;
-export function updateRow(rowId: RowID, row: RowElement): RowID;
+ function insertRow(row: RowElement): RowID;
+ function deleteRow(rowId: RowID): void;
+ function updateRow(rowId: RowID, row: RowElement): RowID;
--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -9,7 +9,7 @@ const row: RowElement = {
 };
 
 const newRowID: RowID = CRUD.insertRow(row);
-
+const updatedRow: RowElement = { ...row, age: 23 };
 const updatedRow: RowElement = {
   firstName: 'Guillaume',
   lastName: 'Salva',
